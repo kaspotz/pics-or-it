d56d9732fc66fed2c1f46b0b9fb853734c63dcc2
k <mja>
2023-08-01 14:10:25 -0500

    new mainnet contract

--- app/src/web3/constants.js
+++ app/src/web3/constants.js
@@ -4,7 +4,7 @@ export const ARB_RPC =
 export const ARB_DEV_RPC =
   'https://arbitrum-goerli.infura.io/v3/91b50d6f88e74281bc3240160cf1b3eb';
 
-export const CONTRACT_ADDRESS = '0xc6ae570a4D2A61BfD6Fe82ef86c72b172297aaEA';
+export const CONTRACT_ADDRESS = '0xdfFE8A4a4103f968Ffd61fd082D08c41dCf9b940';
 export const CONTRACT_DEV_ADDRESS =
   '0x83355E78fEA8147F1CFD533a7A8A1D15Ec22432c';
 
