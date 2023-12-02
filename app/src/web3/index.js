09c5525b2d2378a53f7a4f68ec2caa2a6c8c425d
kaspotz <66429806+kaspotz@users.noreply.github.com>
2023-12-02 15:22:10 -0800

    show all bounties - Update index.js

--- app/src/web3/index.js
+++ app/src/web3/index.js
@@ -193,7 +193,7 @@ export const useContract = () => {
 
         if (allBountiesLength >= 0) {
           // Calculate the starting and ending indices to fetch the latest 20 bounties
-          const startIndex = Math.max(0, allBountiesLength - 79);
+          const startIndex = 0;
           const endIndex = allBountiesLength;
 
           const allBounties = await connectedContract.getBounties(
