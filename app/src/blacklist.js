7b5c01a7e049f004e3389467e83de0450c9b17b9
rhovian <atomicjab@proton.me>
2023-12-16 16:30:51 -0600

    blacklist for shit claims

---
+++ app/src/blacklist.js
@@ -0,0 +1,14 @@
+export const blacklist = [
+    {
+        "bountyId": 218,
+        "claims": [
+            205,
+            206,
+            207,
+            208,
+            209,
+            210,
+            211
+        ]
+    }
+]
