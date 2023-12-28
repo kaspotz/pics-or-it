5050d1c360b65529f0d80021fa2deb54fa07e307
kaspotz <66429806+kaspotz@users.noreply.github.com>
2023-12-27 16:32:05 -0800

    more button spacing

--- app/src/components/Bounties/AllBounties.js
+++ app/src/components/Bounties/AllBounties.js
@@ -262,7 +262,7 @@ function AllBounties({
                   backgroundColor: activeButton === 'first' ? '#c24648' : '#f4595b'
                 }}
               >
-                &emsp;&emsp;open bounties&emsp;&emsp;</button>
+                &emsp;&emsp;&emsp;open bounties&emsp;&emsp;&emsp;</button>
               <button className={`bounty-filter-button ${activeButton === 'second' ? 'active' : ''}`}
                 onClick={() => {
                   bountyFilter(false);
