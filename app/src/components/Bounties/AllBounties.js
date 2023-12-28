abd774e3ff806338372f2669a4dc22b142463381
kaspotz <66429806+kaspotz@users.noreply.github.com>
2023-12-27 16:34:27 -0800

    reducing spacing

--- app/src/components/Bounties/AllBounties.js
+++ app/src/components/Bounties/AllBounties.js
@@ -262,7 +262,7 @@ function AllBounties({
                   backgroundColor: activeButton === 'first' ? '#c24648' : '#f4595b'
                 }}
               >
-                &emsp;&emsp;&emsp;open bounties&emsp;&emsp;&emsp;</button>
+                &emsp;open bounties&emsp;</button>
               <button className={`bounty-filter-button ${activeButton === 'second' ? 'active' : ''}`}
                 onClick={() => {
                   bountyFilter(false);
