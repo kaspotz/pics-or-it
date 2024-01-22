3fc9cbd5a232070cff43ad868b4545ea28071bed
kaspotz <66429806+kaspotz@users.noreply.github.com>
2024-01-21 18:31:23 -0800

    fix successfully - Update CreateClaim.js

--- app/src/components/Claims/CreateClaim.js
+++ app/src/components/Claims/CreateClaim.js
@@ -143,7 +143,7 @@ function CreateClaim({ onClose, bountyId }) {
         setFormData({ name: '', description: '' });
         setStatus({ loading: false, processString: '' });
         setFile(null);
-        toast.info('Claim submitted succesfully');
+        toast.info('Claim submitted successfully');
       })();
     }
   }, [imageUri]);
